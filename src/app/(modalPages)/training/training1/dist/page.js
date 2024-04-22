f60877544b9c75f7865c7ae3a6d7fc28fe071bdd
JohnWJun <tbvjdngus@gmail.com>
2024-04-22 18:22:45 +0900

    "2nd Commit"

--- src/app/(modalPages)/training/training1/dist/page.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Training1_1 = require("@/app/_component/Training1");
-function IntroductionPage() {
-    return (React.createElement(React.Fragment, null,
-        React.createElement(Training1_1["default"], null)));
-}
-exports["default"] = IntroductionPage;
